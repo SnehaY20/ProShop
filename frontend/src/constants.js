bfaf600e9f34342503d0f002d9071e81eaa8906e
SnehaY20 <21snehay@gmail.com>
2024-10-17 16:10:56 +0530

    Added frontend authentication.

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
-// export const BASE_URL =
-//   process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
-export const BASE_URL = "";
+ export const BASE_URL =
+   process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+// export const BASE_URL = "";
 export const PRODUCTS_URL = "http://localhost:5000/api/products";
 export const USERS_URL = "http://localhost:5000/api/users";
 export const ORDERS_URL = "http://localhost:5000/api/orders";
